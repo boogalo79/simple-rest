519380e155dee62747dfffc7f59ad85457442778
aschulz <andischulz@yahoo.de>
2016-11-05 15:47:40 +0100

    initial commit of HelloRest class

---
+++ src/main/java/boogalo79/rs/HelloRest.java
@@ -0,0 +1,13 @@
+package boogalo79.rs;
+
+public class HelloRest{
+
+	public String hello(){
+		return "hello";
+	}
+	
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+	
+}
